cd13a9e1aabef5384ef1861fa05f76c40f00350a
oksola <1650152108@qq.com>
2016-10-13 13:29:56 +0800

    getZetian completed

--- getZetian.py
+++ getZetian.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import os
+import time
 import re
-
+'''
+爬取遮天的小说，下载下来
+'''
 def getHerfAndTitle(url):
     all_list = []
     retu = requests.get(url)
@@ -16,7 +18,13 @@ def getHerfAndTitle(url):
 
 def getContent(all_list):
     reg = re.compile(r'[/\:*?<>"|]+')
+    time_count = 0
     for i in all_list:
+        #防止爬太快而被主机拒绝
+        if time_count >= 50:
+            time.sleep(1)
+            time_count=0
+        time_count += 1
         url = 'http://www.biquku.com/3/3226/'+i[1]
         retu = requests.get(url)
         soup = BeautifulSoup(retu.content.decode('gbk',errors='ignore'),'lxml')
@@ -26,11 +34,10 @@ def getContent(all_list):
         for item in re_list:
             i[0] = i[0].replace(item,'')
         with open('E:\\xiaoshuo\\{0}.html'.format(i[0]),'w') as f:
+            f.write(r'<!DOCTYPE html><html lang="en"><head><meta charset="GBK"><title>Title</title></head><body bgcolor="#deb887">')
             f.write(str(con_list[0]).encode('GBK','ignore').decode('GBK',errors='ignore'))
-        # print(type(con_list[0]))
-        # print(str(con_list[0]))
-        # print(con_list[0])
-        # break
+            f.write(r'</body></html>')
+        print(i)
 
 url = 'http://www.biquku.com/3/3226/index.html'
 all_list = getHerfAndTitle(url)
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import os
 import re
+import requests
+from bs4 import BeautifulSoup
 # open('E:\\xiaoshuo\\text.txt','w')
 '''
 reg = re.compile(r'[/\:*?<>"|]+')
@@ -11,5 +13,13 @@ testStr = testStr.replace(re_list[0],'')
 print(testStr)
 '''
 with open('E:\\xiaoshuo\\test.html','w') as f:
+    f.write(r'<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Title</title></head><body background="green">')
     f.write('1141343<br>')
-    f.write('dahdoa')
+    f.write(r'</body></html>')
+'''
+url = 'http://www.biquku.com/3/3226/2021814.html'
+retu = requests.get(url)
+soup = BeautifulSoup(retu.content.decode('gbk',errors='ignore'),'lxml')
+con_list = soup.find_all('div',attrs={'id':'content'})
+print(con_list[0])
+'''
